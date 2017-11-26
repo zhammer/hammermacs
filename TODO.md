10c056fe9cf455311f5dfab98755cee4c94a99fd
zhammer <zachary_hammer@brown.edu>
2017-11-26 16:36:09 -0500

    Add package config, other features

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+
 # TODO
   * Convert todo to org mode.
   * Emacsclient return focus to terminal after killing: [stackoverflow post](https://stackoverflow.com/questions/47496959/how-to-return-focus-to-terminal-after-closing-gui-emacsclient-on-mac-os-x)
@@ -7,6 +8,7 @@
       * right now C-u C-j decreases indentation by one level, C-u C-u C-j increases
   * correct diff coloring in eshell (for git diff) [stack overflow](https://emacs.stackexchange.com/questions/9517/colored-git-output-in-eshell)
   * would be cool to have a jenkins build on PR's to check that emacs init.el doesn't break.
-  * Add packages to init.el (currently: helm, markdown).
+  * Add packages to init.el (currently: helm, markdown, wrap-region).
       * see [this stack overflow post](https://stackoverflow.com/questions/10092322/how-to-automatically-install-emacs-packages-by-specifying-a-list-of-package-name) or philippe's exordium [setup](https://github.com/philippe-grenet/exordium/blob/master/init.el#L193)
   * Markdown live preview doesn't support lists. Fix?
+  * [Dired Omit Mode](https://www.emacswiki.org/emacs/DiredOmitMode) (with sqk file?)
--- init.el
+++ init.el
@@ -1,13 +1,22 @@
+;; hammermacs init.el
 
-
-;; ~~ add elpa and melpa package archives ~~
+;; ~~ setup packages ~~
 (require 'package)
+(setq package-list '(markdown-mode helm wrap-region))
+;; Question: how do i combine these into one line?
 (add-to-list 'package-archives
 	     '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;; add package list: markdown-mode, helm
 (package-initialize)
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; ~~ no menu, tool buttons, scroll, custom splash ~~
 (menu-bar-mode -1)
@@ -21,6 +30,28 @@
 (delete-selection-mode 1)
 (setq show-paren-delay 0) ;; must come before show-paren-mode enable
 (show-paren-mode 1)
+(electric-pair-mode 1)
+
+;; (defun insert-hard-brackets (&optional arg) ;; copied from insert-parentheses.
+;;   ;; M-{ is already mapped. maybe i should just hook into insert (,[,{ commands.
+;;   ;; if there is a region active, insert-pair of the bracket type
+;;   "Enclose following ARG sexps in parentheses.
+;; Leave point after open-paren.
+;; A negative ARG encloses the preceding ARG sexps instead.
+;; No argument is equivalent to zero: just insert `()' and leave point between.
+;; If `parens-require-spaces' is non-nil, this command also inserts a space
+;; before and after, depending on the surrounding characters.
+;; If region is active, insert enclosing characters at region boundaries.
+
+;; This command assumes point is not in a string or comment."
+;;   (interactive "P")
+;;   (insert-pair arg ?\[ ?\]))
+;; (global-set-key (kbd "M-[") 'insert-hard-brackets)
+
+;; ;; ~~ ido mode ~~
+;; (ido-mode 1)
+;; (setq ido-everywhere t)
+;; (setq ido-enable-flex-matching t)
 
 ;; ~~ theme ~~
 (custom-set-variables
@@ -29,7 +60,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
- '(package-selected-packages (quote (helm-ebdb helm markdown-mode)))
+ '(package-selected-packages (quote (wrap-region helm-ebdb helm markdown-mode)))
  '(x-focus-frame nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
