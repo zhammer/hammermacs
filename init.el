2f2d7ea210dd35c1310cc309c79a444e8bbe0c25
zhammer <zachary_hammer@brown.edu>
2017-11-25 20:26:43 -0500

    Add after init and before init files

--- init.el
+++ init.el
@@ -11,3 +11,15 @@
 (custom-set-variables
  '(custom-enabled-themes (quote (misterioso))))
 (custom-set-faces)
+
+(defconst exordium-before-init "before-init.el"
+  "name of the before init file")
+
+(defconst exordium-prefs "prefs.el"
+  "name of the prefs file")
+
+(defconst exordium-after-init "after-init.el"
+  "name of the after init file")
+
+;; Save any custom set variable in after-init.el rather than at the end of init.el:
+(setq custom-file "~/.emacs.d/after-init.el")
