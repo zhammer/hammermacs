da64caa0b4eb0673a8ebe26f59cac0bc11fe3d98
zhammer <zachary_hammer@brown.edu>
2017-11-26 05:13:44 -0500

    Add markdown support

--- init.el
+++ init.el
@@ -16,11 +16,25 @@
 
 ;; ~~ theme ~~
 (custom-set-variables
- '(custom-enabled-themes (quote (misterioso))))
-(custom-set-faces)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages (quote (markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; ~~ add elpa and melpa package archives ~~
 (setq package-archives  '(("gnu" . "http://elpa.gnu.org/packages/")   ("melpa" . "http://melpa.org/packages/")))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
 
 ;; ~~ pdb command ~~
 (setq gud-pdb-command-name "python -m pdb")
