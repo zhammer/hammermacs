a24d3d17242862abd4adbea2960fca1958ca26e3
Zach Hammer <zhammer1@bloomberg.net>
2017-12-11 17:31:30 -0500

    Update cc init

--- modules/init-cc.el
+++ modules/init-cc.el
@@ -1,14 +1,13 @@
 ;; hammermacs init-cc
 
+;; TODO: update comment mode to indent under function defintions
 ;; TODO: add cpp-switch-h-cpp functionality from exordium
 (defun hmacs-cc-mode-config()
   "For use in `cc-mode-hook'."
   (c-set-style "stroustrup")
   (c-set-offset 'innamespace 0)
-  (c-set-offset 'inextern-lang 0))
+  (c-set-offset 'inextern-lang 0)
+  (setq indent-tabs-mode nil))
 (add-hook 'c-mode-common-hook 'hmacs-cc-mode-config)
-
-;; ~~ helm ~~
-(global-set-key (kbd "C-h a") 'helm-apropos)
 
 (provide 'init-cc)
