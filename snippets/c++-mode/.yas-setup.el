6f39175c3107e5ebbb488427b8ef68e0869b17e3
Zach Hammer <zhammer1@bloomberg.net>
2017-12-13 15:04:28 -0500

    Add c++ snippets

---
+++ snippets/c++-mode/.yas-setup.el
@@ -0,0 +1,38 @@
+;; hmacs c++-mode yas setup
+
+(require 'cl)
+
+(defvar hmacs-file-name-re "\\(?:\\(\\w+\\)_\\)\\(\\w+\\)"
+  "Regex for extracting package name of a file.
+	\\1: package
+	\\2: component
+	\\3: filetype")
+
+(defun hmacs-extract-package-name ()
+  "Get the package name of a cpp filename. If no package name, return empty string."
+  (let ((file-name (file-name-base (or buffer-file-name ""))))
+    (message (concat "File name: " file-name ", Regex: " hmacs-file-name-re))
+    (when (string-match hmacs-file-name-re file-name)
+      (match-string 1 file-name))
+    ))
+
+(defun hmacs-extract-component-name ()
+  "Get the component name of a c++ filename. If no component name, return empty string."
+  (let ((file-name (file-name-base (or buffer-file-name ""))))
+    (if (string-match hmacs-file-name-re file-name)
+	(match-string 2 file-name)
+      nil)
+    ))
+
+(defun hmacs-gen-headerguard-name ()
+  (let ((package-name (hmacs-extract-package-name))
+	(component-name (hmacs-extract-component-name)))
+    (message package-name)
+    (message component-name)
+    (mapconcat 'upcase (remove-if #'null (list package-name component-name)) "_")))
+
+
+;; TODO: get package name
+;; TODO: get component name
+
+;; TODO: get ifndefguard name
