68dbf80e81e1c651b87e8758c6c0d45c3e08042b
Zach Hammer <zhammer1@bloomberg.net>
2017-12-13 15:21:17 -0500

    Update c++ snippets

--- snippets/c++-mode/.yas-setup.el
+++ snippets/c++-mode/.yas-setup.el
@@ -2,7 +2,7 @@
 
 (require 'cl)
 
-(defvar hmacs-file-name-re "\\(?:\\(\\w+\\)_\\)\\(\\w+\\)"
+(defvar hmacs-file-name-re "\\(?:\\(\\w+\\)_\\)?\\(\\w+\\)"
   "Regex for extracting package name of a file.
 	\\1: package
 	\\2: component
@@ -11,7 +11,6 @@
 (defun hmacs-extract-package-name ()
   "Get the package name of a cpp filename. If no package name, return empty string."
   (let ((file-name (file-name-base (or buffer-file-name ""))))
-    (message (concat "File name: " file-name ", Regex: " hmacs-file-name-re))
     (when (string-match hmacs-file-name-re file-name)
       (match-string 1 file-name))
     ))
@@ -27,8 +26,6 @@
 (defun hmacs-gen-headerguard-name ()
   (let ((package-name (hmacs-extract-package-name))
 	(component-name (hmacs-extract-component-name)))
-    (message package-name)
-    (message component-name)
     (mapconcat 'upcase (remove-if #'null (list package-name component-name)) "_")))
 
 
