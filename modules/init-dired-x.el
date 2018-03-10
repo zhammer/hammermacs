ba06e4a35bf73578a7069a1e7f157300929a802e
zhammer <zachary_hammer@brown.edu>
2018-03-10 10:41:20 -0500

    Add __pycache__ to diredx ignore

--- modules/init-dired-x.el
+++ modules/init-dired-x.el
@@ -5,6 +5,7 @@
 ;; TODO: add key command to create non-dir file from dired-mode
 (setq-default dired-omit-files-p t)
 (setq dired-omit-files (concat dired-omit-files
+                               "\\|^__pycache__$"
 			       "\\|^.*~$"
 			       "\\|^.*\\.pyc$"
 			       "\\|^.*\\.o$"
